efce2e00d5dfd63d0a6ff4fa5ad2858389f4f168
jiguixin <jiguixin_1008@qq.com>
2013-03-25 10:10:06 +0800

    第一次添加
    2013.3.25

---
+++ ASP.NET MVC/HtmlHelperExercise/HtmlHelperExercise/Controllers/HtmlExerciseController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlHelperExercise.Controllers
+{
+    public class HtmlExerciseController : Controller
+    {
+        //
+        // GET: /HtmlExercise/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ActionLinkExercise(string UserName)
+        {
+            if (string.IsNullOrEmpty(UserName))
+                return View();
+            else
+            {
+                ViewBag.UserName = UserName;
+                ViewData["Age"] = 10;
+                ViewData.TemplateInfo.HtmlFieldPrefix = "_jimtest";
+                return View("ActionLinkExerciseParameter");
+            }
+        }
+
+    }
+}
